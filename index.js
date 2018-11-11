caaa044ef1b2f700b0cdab9246f38364fcec8153
yochp <peled770@gmail.com>
2018-11-12 00:00:53 +0200

    add func that take the radarP and give for each one a random location and return a object wit the rand XY an Id of the radar

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 
-
 app.get('/', (req, res) => {
     res.send('hello guys')
 });
@@ -14,8 +13,7 @@ io.on('connection', socket => {
     console.log('a user connected to socket');
 
 
-
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 
@@ -24,12 +22,60 @@ io.on('connection', socket => {
     // });
 
 
-    socket.emit('get_radars',radars);
-    socket.emit('get_radarsPosition',radarsPosition);
+    socket.emit('get_radars', radars);
+    socket.emit('get_radarsPosition', radarsPosition);
 
 
 });
 
+
+let getRandomLocation = function (latitude, longitude, radiusInMeters, RadarId) {
+    let id = RadarId;
+
+    let getRandomCoordinates = function (radius) {
+        // Generate two random numbers
+        let a = Math.random(),
+            b = Math.random();
+
+        return [
+            b * radius * Math.cos(2 * Math.PI * a / b),
+            b * radius * Math.sin(2 * Math.PI * a / b)
+        ];
+    };
+
+    let randomCoordinates = getRandomCoordinates(radiusInMeters, true);
+
+    // Earths radius in meters via WGS 84 model.
+    let earth = 6378137;
+
+    // Offsets in meters.
+    let northOffset = randomCoordinates[0],
+        eastOffset = randomCoordinates[1];
+
+    // Offset coordinates in radians.
+    let offsetLatitude = northOffset / earth,
+        offsetLongitude = eastOffset / (earth * Math.cos(Math.PI * (latitude / 180)));
+
+
+    // Offset position in decimal degrees.
+    // console.log(latitude + (offsetLatitude * (180 / Math.PI)) +', ' + longitude + (offsetLongitude * (180 / Math.PI)));
+    return {
+        // longitude: longitude + (offsetLongitude * (180 / Math.PI)),
+        lon: longitude + (offsetLongitude * (180 / Math.PI)),
+        lat: latitude + (offsetLatitude * (180 / Math.PI)),
+        id: id
+
+    }
+};
+
+
+
+setInterval(() => {
+    radarsPosition.forEach(r => {
+        let rXY = getRandomLocation(r.lat, r.lon, 1000, r.id);
+        console.log(rXY);
+    });
+}, 3000);
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
